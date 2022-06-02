c68bfab8da618edebc20d09f36f9b0867d532566
iamdevelopper <97307891+Celesin@users.noreply.github.com>
2022-06-02 10:54:24 +0200

    added file to git repo

--- views.html.py
+++ views.html.py
@@ -4,3 +4,9 @@ this is updates added to the index.html
 
 
 <div>the new feature are being added</div>
+div>this is my first division in the html document</div>
+this is updates added to the index.html
+<div>this is my first division in the html document</div>
+
+
+<div>the new feature
